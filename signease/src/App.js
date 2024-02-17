5e6a616eec6f1c266560f6615c911e5c1b54155c
Brian Zhang <brian.w.zhang@gmail.com>
2024-02-17 11:50:06 -0500

    reorganize css files

--- signease/src/App.js
+++ signease/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Sidebar from './sidebar'; // Make sure the path to Sidebar is correct
 import SearchBar from './searchbar'; // Make sure the path to SearchBar is correct
-import './App.css'; // Assuming you're using CSS for styling
+import './styles/App.css'; // Assuming you're using CSS for styling
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -21,15 +21,24 @@ function App() {
   };
 
   return (
-    <div className="App" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
       <Sidebar />
       <div className="main" style={{ marginLeft: '250px' }}></div>
       <button
-  className={`sidebarToggle ${sidebarOpen ? 'hideArrow' : ''}`}
-  onClick={toggleSidebar}
->
-  {/* The button content can be empty if you're using ::before for the arrow */}
-</button>
+        className={`sidebarToggle ${sidebarOpen ? 'hideArrow' : ''}`}
+        onClick={toggleSidebar}
+      >
+        {/* The button content can be empty if you're using ::before for the arrow */}
+      </button>
       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
       <div className={`main ${sidebarOpen ? 'shifted' : ''}`}></div>
       <h1>SignEase</h1>
