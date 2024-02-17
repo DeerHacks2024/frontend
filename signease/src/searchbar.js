b84de8e808a02ced914b51cd06ebedd6fe7146e0
Ahmed <ahmedm25085@gmail.com>
2024-02-17 13:20:11 -0500

    flaskapicorserror

--- signease/src/searchbar.js
+++ signease/src/searchbar.js
@@ -3,15 +3,72 @@ import React, { useState } from 'react';
 
 function SearchBar({ onSearch }) {
   const [query, setQuery] = useState('');
+  const [postId, setPostId] = useState(null);
 
   const handleChange = (event) => {
     setQuery(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    // try {
+    //   await axios.post('/api/sentences', { sentence: query });
+    //   console.log('Sentence submitted successfully!');
+    // } catch (error) {
+    //   console.error('Error submitting sentence:', error);
+    // }
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: query })
+    };
+    fetch('http://127.0.0.1:5000/api/sentences', requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          // Throw an error if the response is not ok to handle it in the catch block
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Assuming your API returns an object with an ID, adjust accordingly
+        setPostId(data.id); // Update the state with the new ID
+        console.log('Sentence submitted successfully!', data);
+      })
+      .catch(error => {
+        console.error('Error submitting sentence:', error);
+      });
+
+
     const wordsArray = query.split(' ');
     onSearch(wordsArray);
+
+    // const postData = {
+    //     sentence: query
+    // };
+
+    // const requestOptions = {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json' // Specify the content type if sending JSON data
+    //     },
+    //     body: JSON.stringify(postData) // Convert the data to JSON format
+    //   };
+
+      // Make the POST request
+    // fetch('https://deerhacks2024backend.vercel.app/insert', requestOptions)
+    //     .then(response => {
+    //     if (!response.ok) {
+    //         throw new Error('Network response was not ok');
+    //     }
+    //     return response.json();
+    //     })
+    //     .then(data => {
+    //     console.log(data); // Work with the response data here
+    //     })
+    //     .catch(error => {
+    //     console.error('Fetch error:', error);
+    //     });
   };
 
   return (
@@ -29,6 +86,7 @@ function SearchBar({ onSearch }) {
                 Search
             </button>
         </form>
+        {postId && <p>Post ID: {postId}</p>}
     </div>
   );
 }
