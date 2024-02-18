e4a432824cc8e942e53995702e3d388ad2fcf794
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2024-02-18 05:39:11 -0500

    Added database connection endpoint with custom backend

--- signease/src/searchbar.js
+++ signease/src/searchbar.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// Inside SearchBar.js
-// ...
 function SearchBar({ onSearch, surpriseData }) {
   const [query, setQuery] = useState('');
   const [postId, setPostId] = useState(null);
@@ -14,13 +12,27 @@ function SearchBar({ onSearch, surpriseData }) {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
+
     try {
-      // ...
+      const response = await fetch('https://deerhacks2024backend.vercel.app/insert', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ event: query }) // Sending the input text as the event
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to insert event');
+      }
+
+      // If the insertion is successful, log the response
+      const data = await response.json();
+      console.log('Event inserted successfully:', data);
     } catch (error) {
       console.error('Error inserting event:', error);
     }
-    
+
     const wordsArray = query.split(' ');
     onSearch(wordsArray);
 
@@ -29,21 +41,20 @@ function SearchBar({ onSearch, surpriseData }) {
 
   return (
     <div className="searchContainer">
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                className="searchInput"
-                placeholder={surpriseData ? `${surpriseData}` : "Search..."}
-                value={query} 
-                onChange={handleChange}
-            />
-            <button type="submit" className="searchButton">
-              🔍
-            </button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="searchInput"
+          placeholder={surpriseData ? `${surpriseData}` : "Search..."}
+          value={query} 
+          onChange={handleChange}
+        />
+        <button type="submit" className="searchButton">
+          🔍
+        </button>
+      </form>
     </div>
   );
 }
 
 export default SearchBar;
-
