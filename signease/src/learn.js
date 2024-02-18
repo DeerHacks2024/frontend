b68456eb181e89e0021af8dc83ade5ba78aa140f
Ahmed <ahmedm25085@gmail.com>
2024-02-18 05:22:43 -0500

    texttraverse

--- signease/src/learn.js
+++ signease/src/learn.js
@@ -1,5 +1,6 @@
 import { useNavigate, useLocation } from 'react-router-dom';
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { FaHome } from 'react-icons/fa';
 // import './styles/learn.css';
 
 const Learn = () => {
@@ -7,6 +8,16 @@ const Learn = () => {
   const location = useLocation();
   const query = location.state?.query;
   const videoRef = useRef(null);
+  const words = query.split(' '); // Split the query into words
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + words.length) % words.length);
+  };
 
   useEffect(() => {
     // Function to start the camera stream
@@ -32,8 +43,8 @@ const Learn = () => {
   return (
     <div>
       <button onClick={() => navigate('/')}
-      style={{ position: 'absolute', top: '20px', left: '20px', background: 'none', fontSize: '54px', border: 'none' }}>
-      🏠
+      style={{ position: 'absolute', top: '20px', left: '20px', background: 'none', fontSize: '54px', border: 'none', color: 'white', cursor: 'pointer' }}>
+      <FaHome size={50} />
       </button>
       <div className="learnContainer">
         <div className="half leftBox">
@@ -47,11 +58,18 @@ const Learn = () => {
             <div className="videoContainer">
               <video ref={videoRef} autoPlay playsInline style={{ width: '100%', height: 'auto', transform: 'scaleX(-1)' }}></video>
             </div>
-            <div className='sentence-text'>
-            <p>{query}</p>
+            <div className='sentence-text' style={{display: 'flex', fontSize: '25px', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'}}>
+              {words.map((word, index) => (
+                <span key={index} style={{backgroundColor: index === currentIndex ? 'yellow' : 'transparent', marginRight: '5px', color: 'black'}}>
+                  {word}{' '}
+                </span>
+              ))}
+            </div>
+            <div className="button-container" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '10px', marginTop: '10px', height: '30px' }}>
+              <button onClick={handlePrevious}>previous</button>
+              <button onClick={handleNext}>next</button>
             </div>
           </div>
-          
         </div>
     </div>
       {/* Additional content for the Learn page */}
